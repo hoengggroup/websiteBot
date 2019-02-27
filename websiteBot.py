0202b24928503632d223193e3541b200cb9754b8
Niklas Bogensperger <bogensperger.niklas@gmail.com>
2019-02-27 14:22:14 +0100

    Update websiteBot.py
    New:
    Device specific settings
    
    Changes:
    Fix indentation (inconsistent tabs/spaces)
    Major cleanup/reorganization
    Slight reformat of some Telegram messages
    Set log level of free room to warning for better visibility (keep?)
    Bump version to 2.0 because of new feature

--- websiteBot.py
+++ websiteBot.py
@@ -1,7 +1,6 @@
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 
 import urllib.request
 from requests import get
@@ -10,157 +9,157 @@ import logging
 import sys
 from random import randint
 
-from pyvirtualdisplay import Display  # ONLY FOR RPI
-
 from sendTelegram import bot_sendtext
 
 
-debug = False  # DISABLE WHEN NOT DEBUGGING!!!
+# CHECK THESE VARIABLES BEFORE DEPLOYMENT!
+# metadata
+device = "RPI"
+version = "2.0"
+# initializations
 loop = True
-version = 1.4
+parsingMode = -1
+# website
+websiteURL = "http://reservation.livingscience.ch/wohnen"
+# timing
+aliveSignalThreshold = 1800
+minSleepTime = 45
+maxSleepTime = 90
+# debugging
+debug = False
+debugLoopCounter = 0
+debugLoopCounterMax = 1
+localDebugURL = ""
 
-
-# ONLY FOR RPI
-display = Display(visible=0, size=(1024, 768))
-display.start()
 
 # log setup
 # create logger
 logger = logging.getLogger('server_log')
 logger.setLevel(logging.DEBUG)
-
 # create file handler and set it to DEBUG level
 fh = logging.FileHandler('server_debug.log')
 fh.setLevel(logging.DEBUG)
-
 # create file handler and set it to INFO level
 fh2 = logging.FileHandler('server_info.log')
 fh2.setLevel(logging.INFO)
-
 # create console handler and set it to DEBUG level
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
-
 # create formatter and add it to the handlers
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
 fh2.setFormatter(formatter)
 ch.setFormatter(formatter)
-
 # add the handlers to the logger
 logger.addHandler(fh)
 logger.addHandler(fh2)
 logger.addHandler(ch)
 
 
-URL_TO_SCAN = "http://reservation.livingscience.ch/wohnen"
-URL_TO_SCAN_LOCAL = "file:///C:/Users/bingo/polybox/Shared/HWW/livingscience haupt.html"
+# specify imports and selenium drivers for various devices
+if device == "RPI":
+    from pyvirtualdisplay import Display
+    from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+
+    display = Display(visible=0, size=(1024, 768))
+    display.start()
+
+    firefoxProfile = FirefoxProfile()
+    firefoxProfile.set_preference("browser.privatebrowsing.autostart", True)
+    driver = webdriver.Firefox(firefox_profile=firefoxProfile)
+elif device == "manual_firefox":
+    from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+
+    firefoxProfile = FirefoxProfile()
+    firefoxProfile.set_preference("browser.privatebrowsing.autostart", True)
+    driver = webdriver.Firefox(firefox_profile=firefoxProfile)
+elif device == "manual_chrome":
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--incognito")
+    driver = webdriver.Chrome(executable_path=r"/usr/local/bin/chromedriver", chrome_options=chrome_options)
+else:
+    logger.error("Invalid device type. Exiting.")
+    sys.exit()
 
 
-logger.info("Starting up. Current version is: " + version)
-bot_sendtext("debug", "Starting up. Current version is: " + version)
-
-firefoxProfile = FirefoxProfile()
-# set private mode (as standard)
-firefoxProfile.set_preference("browser.privatebrowsing.autostart", True)
-
-driverFire = webdriver.Firefox(firefox_profile=firefoxProfile)
-
-last_alive_signal_time = int(time.time())  # last time when alive signal sent
+# startup
+logger.info("Starting up. Current version is: " + version + " Device is: " + device)
+bot_sendtext("debug", "Starting up.\nCurrent version is: " + version + "\nDevice is: " + device)
+lastAliveSignalTime = int(time.time())  # last time when alive signal was sent
 
 
-last_alive_signal_thresh = 1800  # SET THIS FOR DEPLOYMENT!!!
-minSleepTime = 45  # SET THIS FOR DEPLOYMENT!!!
-maxSleepTime = 90  # SET THIS FOR DEPLOYMENT!!!
-
-
-# for debugging
-whileLoopCounter = 0
-whileLoopCounterMax = 1
-
-
-# check ip
+# initial IP address check
 startup_ip = "0.0.0.0"
 try:
-    ip = get('https://api.ipify.org').text
-    startup_ip = ip
+    startup_ip = get('https://api.ipify.org').text
     logger.info("Startup IP address is: " + startup_ip)
     bot_sendtext("debug", "Startup IP address is: " + startup_ip)
 except Exception as e:
     logger.error("An unknown exception has occured in the initial IP checker subroutine. Error: " + e)
-    bot_sendtext("debug", "An unknown exception has occured in the initial IP checker subroutine. Error: " + e)
+    bot_sendtext("debug", "An unknown exception has occured in the initial IP checker subroutine.\nError: " + e)
     loop = False
 
 
 while loop:
     logger.info("Waking up from sleep and starting next while-loop.")
-    # ONLY DEBUG!!!
+
+    # test with local saved webpage when debugging
     if debug:
-        whileLoopCounter += 1
-        if whileLoopCounter == whileLoopCounterMax:
-            URL_TO_SCAN = URL_TO_SCAN_LOCAL
+        debugLoopCounter += 1
+        if debugLoopCounter == debugLoopCounterMax:
+            websiteURL = localDebugURL
 
-
-    # check IP address
+    # subsequent IP address check
     try:
         current_ip = get('https://api.ipify.org').text
         if current_ip != startup_ip:
             logger.error("IP address has changed. New address is " + current_ip + ", while startup IP was " + startup_ip)
-            bot_sendtext("debug", "IP address has changed. New address is " + current_ip + ", while startup IP was " + startup_ip)
-			break
+            bot_sendtext("debug", "IP address has changed.\nNew address is " + current_ip + ", while startup IP was " + startup_ip)
+            break
     except Exception as e:
         logger.error("An unknown exception has occured in the IP checker subroutine. Error: " + e)
-        bot_sendtext("debug", "An unknown exception has occured in the IP checker subroutine. Error: " + e)
+        bot_sendtext("debug", "An unknown exception has occured in the IP checker subroutine.\nError: " + e)
         break
 
-
     # open website
-    driverFire.get(URL_TO_SCAN)
-
-    mode = -1
-
+    driver.get(websiteURL)
 
     # check for nodata field
     try:
-        nodata_field = driverFire.find_element(By.CLASS_NAME, "nodata")
+        nodata_field = driver.find_element(By.CLASS_NAME, "nodata")
         logger.debug("nodata text field: found. Text: " + nodata_field.text)
-        mode = 0
+        parsingMode = 0
     except selenium.common.exceptions.NoSuchElementException:
         logger.info("nodata text field: NOT found.")
-        mode = 1
+        parsingMode = 1
     except Exception as e:
         logger.error("An unknown exception has occured in the nodata-field-search subroutine. Error: " + e)
-        bot_sendtext("debug", "An unknown exception has occured in the nodata-field-search subroutine. Error: : " + e)
+        bot_sendtext("debug", "An unknown exception has occured in the nodata-field-search subroutine.\nError: : " + e)
         break
-
 
     # check for whgnr field
     try:
-        rowWhgnr_field = driverFire.find_element_by_xpath('/html/body/div/div[4]/div[2]/div[2]/div[1]/div/div[2]/div/div/div/div/div[3]/div[2]/span[2]')
+        rowWhgnr_field = driver.find_element_by_xpath('/html/body/div/div[4]/div[2]/div[2]/div[1]/div/div[2]/div/div/div/div/div[3]/div[2]/span[2]')
         logger.info("whgnr text field: found. Text: " + rowWhgnr_field.text)
-
-        if mode != 1:  # i.e. mode is 0 i.e. nodata field was found
-            logger.error("Modes do not match. Mode is " + str(mode) + " but expected mode 1.")
-            bot_sendtext("debug", "Modes do not match. Mode is " + str(mode) + " but expected mode 1.")
+        if parsingMode != 1:  # i.e. parsing mode is 0 i.e. nodata field was found
+            logger.error("Modes do not match. Mode is " + str(parsingMode) + " but expected mode 1.")
+            bot_sendtext("debug", "Modes do not match. Mode is " + str(parsingMode) + " but expected mode 1.")
             break
-
-        logger.info("Free room: " + rowWhgnr_field.text)
-        bot_sendtext("shoutout", "Free room: " + rowWhgnr_field.text + "\n" + URL_TO_SCAN)
+        logger.warning("Free room: " + rowWhgnr_field.text)
+        bot_sendtext("shoutout", "Free room: " + rowWhgnr_field.text + "\n" + websiteURL)
     except selenium.common.exceptions.NoSuchElementException:
         logger.debug("whgnr text field: NOT found.")
-
-        if mode != 0:  # i.e. mode is 1 i.e. nodata field was NOT found
-            logger.error("Modes do not match. Mode is " + str(mode) + " but expected mode 0.")
-            bot_sendtext("debug", "Modes do not match. Mode is " + str(mode) + " but expected mode 0.")
+        if parsingMode != 0:  # i.e. parsing mode is 1 i.e. nodata field was NOT found
+            logger.error("Modes do not match. Mode is " + str(parsingMode) + " but expected mode 0.")
+            bot_sendtext("debug", "Modes do not match. Mode is " + str(parsingMode) + " but expected mode 0.")
             break
     except Exception as e:
         logger.error("An unknown exception has occured in the whgnr-field-search subroutine. Error: : " + e)
-        bot_sendtext("debug", "An unknown exception has occured in the whgnr-field-search subroutine. Error: : " + e)
+        bot_sendtext("debug", "An unknown exception has occured in the whgnr-field-search subroutine.\nError: : " + e)
         break
 
-
     # get http response code
-    with urllib.request.urlopen(URL_TO_SCAN) as url:
+    with urllib.request.urlopen(websiteURL) as url:
         httpResponseCode = url.getcode()
         if httpResponseCode == 200:
             logger.debug("URL response code: " + str(httpResponseCode) + ", OK.")
@@ -169,22 +168,24 @@ while loop:
             bot_sendtext("debug", "Retrieve error. URL response code is " + str(httpResponseCode) + " but expected 200.")
             break
 
-
     # alive signal maintainer
-    if int(time.time()) - last_alive_signal_time > last_alive_signal_thresh:
+    if int(time.time()) - lastAliveSignalTime > aliveSignalThreshold:
         logger.debug("Still alive.")
         bot_sendtext("debug", "Still alive.")
-        last_alive_signal_time = int(time.time())
+        lastAliveSignalTime = int(time.time())
 
+    # sleeping
     sleepTime = randint(minSleepTime, maxSleepTime)
-
     logger.debug("Sleeping for " + str(sleepTime) + " seconds.")
-
     time.sleep(sleepTime)
 
 
-driverFire.quit()
-display.stop()
+# cleanup
+driver.quit()
+if device == "RPI":
+    display.stop()
 
+# shutdown
 logger.info("Shutting down.")
 bot_sendtext("debug", "Shutting down.")
+sys.exit()
