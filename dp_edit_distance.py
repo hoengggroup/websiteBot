f4acb5e79294949efd2bacf1dff4e9b042003220
Tassilo Schwarz <black@TS-MacBook-Air-2.local>
2019-05-02 20:11:28 +0200

    update

--- dp_edit_distance.py
+++ dp_edit_distance.py
@@ -4,17 +4,14 @@
 # b=['eins','zwei','drei','vier','sechs','sieben']
 #b=['eins','drei','vier','fuenf','sechs','sieben']
 
-a=['eins','zwei','drei']
-b =['ein','zwei','drei','vier',]
 
+def get_edit_distance_changes(text_old,text_new):
+    tb = [[0 for i in range(len(text_old)+1)] for j in range(len(text_new)+1)]
 
-def get_distance(a,b):
-    tb = [[0 for i in range(len(a)+1)] for j in range(len(b)+1)]
-
-    for i in range(1,len(b)+1):
-        for j in range(1,len(a)+1):
+    for i in range(1,len(text_new)+1):
+        for j in range(1,len(text_old)+1):
             delta=0
-            if(a[j-1] != b[i-1]):
+            if(text_old[j-1] != text_new[i-1]):
                 delta = 1
             tb[i][j] = min(tb[i-1][j],tb[i][j-1],tb[i-1][j-1])+delta
     
@@ -28,12 +25,12 @@ def get_distance(a,b):
     
 
     # backtracking
-    i = len(b)
-    j = len(a)
+    i = len(text_new)
+    j = len(text_old)
 
 
     counter = 0
-    while(counter< len(a)+1+len(b)+1):
+    while(counter< len(text_old)+1+len(text_new)+1):
         counter=counter+1
         
     
@@ -46,7 +43,7 @@ def get_distance(a,b):
             if(min_before < tb [i][j]):
                 # swapped:
                 print("swap")
-                reverse_change_stack.extend([("swap",a[j-1],b[i-1])])
+                reverse_change_stack.extend([("swap",text_old[j-1],text_new[i-1])])
             
             i = i-1
             j = j-1
@@ -56,7 +53,7 @@ def get_distance(a,b):
         elif(tb[i-1][j] == min_before and i-1 >=0): 
             if(min_before < tb[i][j]):
                 print("add")
-                reverse_change_stack.extend([("added",b[i-1])])
+                reverse_change_stack.extend([("added",text_new[i-1])])
             i = i-1
             continue
 
@@ -64,7 +61,7 @@ def get_distance(a,b):
         elif(tb[i][j-1] == min_before and j-1>=0):
             if(min_before < tb[i][j]):
                 print("del")
-                reverse_change_stack.extend([("deleted",a[j-1])])
+                reverse_change_stack.extend([("deleted",text_old[j-1])])
             
             j = j-1
             continue
@@ -74,8 +71,17 @@ def get_distance(a,b):
     return reverse_change_stack[::-1]
                 
 
-changes = []
-changes = get_distance(a,b)
+a=[]
+b=[]
+text_old="eins zwei drei"
+text_new="ein zwei drei vier"
+for word in text_old.split():
+    a.append(word)
+for word in text_new.split():
+    b.append(word)
+
+
+changes = get_edit_distance_changes(a,b)
 for a in changes:
         for my_str in a:
             print(str(my_str),end=' ')
