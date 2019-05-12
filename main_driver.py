b5c827c87e4ea8faff85a23a37f88358a1cf693c
Tassilo Schwarz <black@student-net-vpn-2767.ethz.ch>
2019-05-13 00:13:24 +0200

    beta v4
    Todo:
     - send message (see todo) has to be finished
    - send alive signal to service systemd
    
     rest is working!!

--- main_driver.py
+++ main_driver.py
@@ -3,6 +3,7 @@
 
 import selenium
 from selenium import webdriver
+import hashlib
 
 import time
 import datetime
@@ -45,6 +46,7 @@ driver = webdriver.Firefox(desired_capabilities=caps, executable_path=parent_dir
 driver.set_page_load_timeout(5)
 
 print("hi2")
+
 class Webpage:
     # constant
     url = ""
@@ -86,10 +88,8 @@ def string_to_wordlist(str_to_convert):
 
 
 # 1. load from file
-webpages_dict ={} # pickle.load(open("save.p","rb"))
-myWebpage = Webpage("https://tassilo-schwarz.com/test-page/",30)
-myWebpage.add_chat_id("234")
-webpages_dict["Tassilo Test"] = myWebpage
+webpages_dict =pickle.load(open("save.p","rb"))
+
 
 print("Webpages loaded from file:")
 for myKey in webpages_dict:
@@ -99,7 +99,7 @@ print("Finished __ webpages loaded from file:")
 
 '''
 to add
-myWebpage = Webpage("https://tassilo-schwarz.com/test-page/",30)
+myWebpage = Webpage("https://tassilo-schwarz.com/test-page/",15)
 myWebpage.add_chat_id("234")
 webpages_dict["Tassilo Test"] = myWebpage
 '''
@@ -108,78 +108,78 @@ webpages_dict["Tassilo Test"] = myWebpage
 
 
 
+while(True):
+    for current_wpbg_name in webpages_dict:
 
-for current_wpbg_name in webpages_dict:
+        current_wbpg = webpages_dict[current_wpbg_name]
 
-    current_wbpg = webpages_dict[current_wpbg_name]
+        current_time = datetime.datetime.now()
+        elapsed_time = current_time - current_wbpg.last_time_checked
 
-    current_time = datetime.datetime.now()
-    elapsed_time = current_time - current_wbpg.last_time_checked
+        if(elapsed_time.total_seconds() > current_wbpg.t_sleep):
+            logger.debug("checking website "+current_wpbg_name+" with url "+current_wbpg.url+". ")
 
-    if(elapsed_time > datetime.timedelta(seconds = current_wbpg.t_sleep)):
-        logger.debug("checking website "+current_wpbg_name+" with url "+current_wbpg.url+". ")
+            # 1. get website
+            try:
+                # open website
+                logger.debug("Getting website")
+                driver.get(current_wbpg.url)
+                logger.debug("Got website")
+            except selenium.common.exceptions.TimeoutException as e:
+                #logger.error("TimeoutException has occured in the get website subroutine. Sleeping now for " + str(sleep_time_on_network_error) + "s; retrying then.")
+                logger.error("The error is: " + str(e))
+                #bot_sendtext("debug", logger, "TimeoutException has occured in the get website subroutine. Sleeping now for " + str(sleep_time_on_network_error) + "s; retrying then.")
+                #mode = mode_wait_on_net_error
+                continue
+            except selenium.common.exceptions.WebDriverException as e:
+                #logger.error("WebDriverException has occured in the get website subroutine. Sleeping now for " + str(sleep_time_on_network_error) + "s; retrying then.")
+                logger.error("The error is: " + str(e))
+                # bot_sendtext("debug", logger, "WebDriverException has occured in the get website subroutine. Sleeping now for " + str(sleep_time_on_network_error) + "s; retrying then.")
+                #mode = mode_wait_on_net_error
+                continue
+            except:
+                logger.error("An UNKNOWN exception has occured in the get website subroutine.")
+                logger.error("The error is: Arg 0: " + str(sys.exc_info()[0]) + " Arg 1: " + str(sys.exc_info()[1]) + " Arg 2: " + str(sys.exc_info()[2]))
+                #mode = mode_wait_on_net_error
+                continue
 
-        # 1. get website
-        try:
-            # open website
-            logger.debug("Getting website")
-            driver.get(current_wbpg.url)
-            logger.debug("Got website")
-        except selenium.common.exceptions.TimeoutException as e:
-            #logger.error("TimeoutException has occured in the get website subroutine. Sleeping now for " + str(sleep_time_on_network_error) + "s; retrying then.")
-            logger.error("The error is: " + str(e))
-            #bot_sendtext("debug", logger, "TimeoutException has occured in the get website subroutine. Sleeping now for " + str(sleep_time_on_network_error) + "s; retrying then.")
-            #mode = mode_wait_on_net_error
-            continue
-        except selenium.common.exceptions.WebDriverException as e:
-            #logger.error("WebDriverException has occured in the get website subroutine. Sleeping now for " + str(sleep_time_on_network_error) + "s; retrying then.")
-            logger.error("The error is: " + str(e))
-            # bot_sendtext("debug", logger, "WebDriverException has occured in the get website subroutine. Sleeping now for " + str(sleep_time_on_network_error) + "s; retrying then.")
-            #mode = mode_wait_on_net_error
-            continue
-        except:
-            logger.error("An UNKNOWN exception has occured in the get website subroutine.")
-            logger.error("The error is: Arg 0: " + str(sys.exc_info()[0]) + " Arg 1: " + str(sys.exc_info()[1]) + " Arg 2: " + str(sys.exc_info()[2]))
-            #mode = mode_wait_on_net_error
-            continue
+            # 2. hash website text
+            current_text =  driver.find_element_by_tag_name("body").text
+            current_hash = (hashlib.md5(current_text.encode())).hexdigest()
 
-        # 2. hash website text
-        current_text =  driver.find_element_by_tag_name("body").text
-        current_hash = hash(current_text)
+            # 3. if different:
+            if(current_hash != current_wbpg.last_hash):
+                logger.info("Website hash different. Current: "+str(current_hash)+" vs old hash: "+str(current_wbpg.last_hash))
+                print("Strings equal?"+str(current_wbpg.last_content == current_text))
+                # 3.1 determine difference using DP (O(m * n) ^^)
+                old_words_list = string_to_wordlist(current_wbpg.last_content)
+                new_words_list = string_to_wordlist(current_text)
 
-        # 3. if different:
-        if(current_hash != current_wbpg.last_hash):
-            logger.info("Website hash different.")
-            # 3.1 determine difference using DP (O(m * n) ^^)
-            old_words_list = string_to_wordlist(current_wbpg.current_text)
-            new_words_list = string_to_wordlist(current_text)
+                changes = dp_edit_distance.get_edit_distance_changes(old_words_list, new_words_list)
+                logger.info("Website word difference is: "+str(changes))
+                print("Changes begin ---")
+                for change_tupel in changes:
+                    for my_str in change_tupel:
+                        print(str(my_str), end=' ')
+                    print()
+                print("--- End of changes. ---")
 
-            changes = dp_edit_distance.get_edit_distance_changes(old_words_list, new_words_list)
-            logger.info("Website word difference is: "+str(changes))
-            print("Changes begin ---")
-            for change_tupel in changes:
-                for my_str in change_tupel:
-                    print(str(my_str), end=' ')
-                print()
-            print("--- End of changes. ---")
+                # 3.2 notify world about changes
+                # TODO
+                # - iterate over list of chat ids and send message to them
 
-            # 3.2 notify world about changes
-            # TODO
+                # 3.3 update vars of wbpg object
+                current_wbpg.last_hash = current_hash
+                current_wbpg.last_content = current_text
+            
+            # 4. update time last written
+            current_wbpg.last_time_checked = datetime.datetime.now()
+    
+    # save back to file
+    pickle.dump(webpages_dict,open("save.p","wb"))
 
-            # 3.3 update vars of wbpg object
-            current_wbpg.current_hash = current_hash
-            current_wbpg.current_text = current_text
-
-
-
-        
-
-
-
-
-# save back to file
-pickle.dump(webpages_dict,open("save.p","wb"))
-
+    # sleep now
+    time.sleep(10)
 
 
 print("eof")
