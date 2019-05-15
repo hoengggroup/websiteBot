2cf53706ff95492f955c2e6241e4858d2d0cb89e
Niklas Bogensperger <bogensperger.niklas@gmail.com>
2019-05-15 18:21:07 +0200

    Add functionality to telegramService, type safety (does this exist in Python?, I just added some missing str()), PEP8, general cleanup
    Todo: "active" function in telegramService and counterparts in main_driver; after interrupt and restart of bot the telegramService becomes unreliable and the whole IDE needs to be restarted -> implement proper cleanups on interrrupt/exit; "stop" function in telegramService does not work yet.

--- main_driver.py
+++ main_driver.py
@@ -3,21 +3,21 @@
 
 import selenium
 from selenium import webdriver
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
 import hashlib
 
+import pickle  # to save webpage list
+
+import sys
 import time
 import datetime
 from datetime import date
-import sys
 from random import randint
 from pathlib import Path
-from selenium import webdriver
-from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from pathlib import Path
-import pickle # to save webpage list
 
-# our dependecies
+# our own libraries/dependencies
 from loggerConfig import create_logger
 import dp_edit_distance
 import telegramService
@@ -57,8 +57,8 @@ class Webpage:
 
     def __init__(self, url, t_sleep):
         self.url = url
-        self.t_sleep = t_sleep # sleeping time in seconds
-        self.last_time_checked = datetime.datetime(1999, 2, 28, 23, 23, 53, 952623) # init with very old value
+        self.t_sleep = t_sleep  # sleeping time in seconds
+        self.last_time_checked = datetime.datetime.min  # (1999, 2, 28, 23, 23, 53, 952623)  # init with very old value
 
         # to config aka must get/set via methods
         self.chat_ids = set()
@@ -66,35 +66,35 @@ class Webpage:
         # used while running
         self.last_hash = ""
         self.last_content = ""
-    
+
     def get_chat_ids(self):
         return self.chat_ids
-    
+
     def get_url(self):
         return self.url
 
-    def set_last_hash(self,new_last_hash):
+    def set_last_hash(self, new_last_hash):
         self.last_hash = new_last_hash
-    
+
     def get_last_hash(self):
         return self.last_hash
 
-    def set_last_content(self,new_last_content):
+    def set_last_content(self, new_last_content):
         self.last_content = new_last_content
-    
+
     def get_last_content(self):
         return self.last_content
-    
+
     def set_t_sleep(self, new_t_sleep):
         self.t_sleep = new_t_sleep
-        logger.info("Set new t_sleep for "+str(self.url) +" to "+str(self.t_sleep))
+        logger.info("Set new t_sleep for " + str(self.url) + " to: " + str(self.t_sleep))
 
     def get_t_sleep(self):
         return self.t_sleep
 
-    def set_last_time_checked(self,new_last_time_checked):
+    def set_last_time_checked(self, new_last_time_checked):
         self.last_time_checked = new_last_time_checked
-    
+
     def get_last_time_checked(self):
         return self.last_time_checked
 
@@ -102,26 +102,24 @@ class Webpage:
         try:
             self.chat_ids.add(chat_id_to_add)
             safe_websites_dict()
-            logger.info("Added chat ID "+str(chat_id_to_add) +" to "+str(self.url))
+            logger.info("Added chat ID " + str(chat_id_to_add) + " to: " + str(self.url))
             return True
         except KeyError:
             safe_websites_dict()
-            logger.info("Failed to add chat ID "+str(chat_id_to_add) +" to "+str(self.url))
+            logger.info("Failed to add chat ID " + str(chat_id_to_add) + " to: "+str(self.url))
             return False
-    
 
     def remove_chat_id(self, chat_id_to_remove):
         try:
             self.chat_ids.remove(chat_id_to_remove)
             safe_websites_dict()
-            logger.info("Removed chat ID "+str(chat_id_to_remove) +" from "+str(self.url))
+            logger.info("Removed chat ID " + str(chat_id_to_remove) + " from: " + str(self.url))
             return True
         except KeyError:
             safe_websites_dict()
-            logger.info("Failed to remove chat ID "+str(chat_id_to_remove) +" from "+str(self.url))
+            logger.info("Failed to remove chat ID " + str(chat_id_to_remove) + " from: " + str(self.url))
             return False
 
-    
 
 def string_to_wordlist(str_to_convert):
     my_ret = []
@@ -129,33 +127,33 @@ def string_to_wordlist(str_to_convert):
         my_ret.append(word)
     return my_ret
 
+
 def safe_websites_dict():
     # save back to file
-    pickle.dump(webpages_dict,open("save.p","wb"))
+    pickle.dump(webpages_dict, open("save.p", "wb"))
+
 
 # 1. load from file
-webpages_dict = pickle.load(open("save.p","rb"))
-
+webpages_dict = pickle.load(open("save.p", "rb"))
 
 print("Webpages loaded from file:")
 for myKey in webpages_dict:
     myw = webpages_dict[myKey]
-    print(myKey+": "+ myw.get_url())
+    print(myKey + ": " + myw.get_url())
     print(type(next(iter(myw.get_chat_ids()), None)))
-    print("Chat IDs: "+str(myw.get_chat_ids()))
+    print("Chat IDs: " + str(myw.get_chat_ids()))
 print("Finished __ webpages loaded from file:")
 
 
 '''
 to add
 myWebpage = Webpage("https://google.com",15)
-webpages_dict["GoogleMain"] = myWebpage'''
-
+webpages_dict["GoogleMain"] = myWebpage
+'''
 
 
 # update reference in telegramService
 telegramService.set_webpages_dict_reference(webpages_dict)
-
 
 
 while(True):
@@ -166,53 +164,54 @@ while(True):
         current_time = datetime.datetime.now()
         elapsed_time = current_time - current_wbpg.get_last_time_checked()
 
-        if(elapsed_time.total_seconds() > current_wbpg.get_t_sleep()):
-            logger.debug("checking website " + current_wpbg_name+" with url "+current_wbpg.get_url()+". ")
+        if elapsed_time.total_seconds() > current_wbpg.get_t_sleep():
+            logger.debug("Checking website " + current_wpbg_name + " with url: " + current_wbpg.get_url())
 
             # 1. get website
             try:
                 # open website
-                logger.debug("Getting website")
+                logger.debug("Getting website.")
                 driver.get(current_wbpg.get_url())
-                logger.debug("Got website")
+                logger.debug("Got website.")
             except selenium.common.exceptions.TimeoutException as e:
-                #logger.error("TimeoutException has occured in the get website subroutine. Sleeping now for " + str(sleep_time_on_network_error) + "s; retrying then.")
+                # logger.error("TimeoutException has occured in the get website subroutine. Sleeping now for " + str(sleep_time_on_network_error) + "s; retrying then.")
                 logger.error("The error is: " + str(e))
-                #bot_sendtext("debug", logger, "TimeoutException has occured in the get website subroutine. Sleeping now for " + str(sleep_time_on_network_error) + "s; retrying then.")
-                #mode = mode_wait_on_net_error
+                # bot_sendtext("debug", logger, "TimeoutException has occured in the get website subroutine. Sleeping now for " + str(sleep_time_on_network_error) + "s; retrying then.")
+                # mode = mode_wait_on_net_error
                 continue
             except selenium.common.exceptions.WebDriverException as e:
-                #logger.error("WebDriverException has occured in the get website subroutine. Sleeping now for " + str(sleep_time_on_network_error) + "s; retrying then.")
+                # logger.error("WebDriverException has occured in the get website subroutine. Sleeping now for " + str(sleep_time_on_network_error) + "s; retrying then.")
                 logger.error("The error is: " + str(e))
                 # bot_sendtext("debug", logger, "WebDriverException has occured in the get website subroutine. Sleeping now for " + str(sleep_time_on_network_error) + "s; retrying then.")
-                #mode = mode_wait_on_net_error
+                # mode = mode_wait_on_net_error
                 continue
             except:
                 logger.error("An UNKNOWN exception has occured in the get website subroutine.")
                 logger.error("The error is: Arg 0: " + str(sys.exc_info()[0]) + " Arg 1: " + str(sys.exc_info()[1]) + " Arg 2: " + str(sys.exc_info()[2]))
-                #mode = mode_wait_on_net_error
+                # mode = mode_wait_on_net_error
                 continue
 
             # 2. hash website text
-            current_text =  driver.find_element_by_tag_name("body").text
+            current_text = driver.find_element_by_tag_name("body").text
             current_hash = (hashlib.md5(current_text.encode())).hexdigest()
 
-            # 3. if different:
-            if(current_hash != current_wbpg.get_last_hash()):
-                logger.info("Website hash different. Current: "+str(current_hash)+" vs old hash: "+str(current_wbpg.last_hash))
-                print("Strings equal?"+str(current_wbpg.get_last_content() == current_text))
+            # 3. if different
+            if current_hash != current_wbpg.get_last_hash():
+                logger.info("Website hash different. Current: " + str(current_hash) + " vs old hash: " + str(current_wbpg.last_hash))
+                print("Strings equal?" + str(current_wbpg.get_last_content() == current_text))
+
                 # 3.1 determine difference using DP (O(m * n) ^^)
                 old_words_list = string_to_wordlist(current_wbpg.get_last_content())
                 new_words_list = string_to_wordlist(current_text)
 
-                msg_to_send=""
+                msg_to_send = ""
                 changes = dp_edit_distance.get_edit_distance_changes(old_words_list, new_words_list)
-                logger.info("Website word difference is: "+str(changes))
+                logger.info("Website word difference is: " + str(changes))
                 print("Changes begin ---")
                 for change_tupel in changes:
                     for my_str in change_tupel:
                         print(str(my_str), end=' ')
-                        msg_to_send += (my_str+" ")
+                        msg_to_send += (my_str + " ")
                     print()
                     msg_to_send += "\n"
                 print("--- End of changes. ---")
@@ -220,7 +219,7 @@ while(True):
                 # 3.2 notify world about changes
                 # TODO
                 for current_chat_id in current_wbpg.get_chat_ids():
-                    telegramService.handler(current_chat_id,msg_to_send)
+                    telegramService.handler(current_chat_id, msg_to_send)
                 # - iterate over list of chat ids and send message to them
 
                 # 3.3 update vars of wbpg object
@@ -228,17 +227,12 @@ while(True):
                 current_wbpg.set_last_content(current_text)
 
             # 4. update time last written
-            current_wbpg.set_last_time_checked (datetime.datetime.now())
+            current_wbpg.set_last_time_checked(datetime.datetime.now())
 
     # save back to file
-    pickle.dump(webpages_dict,open("save.p","wb"))
+    pickle.dump(webpages_dict, open("save.p", "wb"))
 
     # sleep now
     time.sleep(10)
 
-
-
-
-
 print("eof")
-
--- telegramService.py
+++ telegramService.py
@@ -1,72 +1,104 @@
 # -*- coding: utf-8 -*-
 
 import telegram
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, BaseFilter
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-'''
-class FilterWebpages(BaseFilter):
-    def filter(self, message):
-        return any(list(main_driver.webpages_dict.keys()) in message.text)
-        #return 'Tassilo Test' in message.text
-'''
-
-webpages_dict = {}
 
 def set_webpages_dict_reference(the_webpages_dict_reference):
     global webpages_dict
     webpages_dict = the_webpages_dict_reference
 
-def start(update, context):
 
+def start(update, context):
     list_webpages = list(webpages_dict.keys())
-    context.bot.send_message(chat_id=update.message.chat_id, text=list_webpages)
+    message_list = '\n- '
+    message_list += '\n- '.join(list_webpages)
+    context.bot.send_message(chat_id=update.message.chat_id, text='Welcome. Here is the list of available webpages:', parse_mode='HTML')
+    context.bot.send_message(chat_id=update.message.chat_id, text=message_list, parse_mode='HTML')
 
 
 def subscribe(update, context):
     webpage = ' '.join(context.args)
-    if(not (webpage in webpages_dict)):
-        context.bot.send_message(chat_id=update.message.chat_id, text="Error. Webpage "+str(webpage)+" doesn't exist")
-        return
-
-    webpage_object = webpages_dict[webpage]
-
-    if webpage_object.add_chat_id(chat_id_to_add = update.message.chat_id):
-        context.bot.send_message(chat_id=update.message.chat_id, text='You have subscribed to webpage: ' + webpage)
+    if webpage in webpages_dict:
+        webpage_object = webpages_dict[webpage]
+    elif webpage:
+        context.bot.send_message(chat_id=update.message.chat_id, text='Error. Webpage ' + str(webpage) + ' does not exist in list.', parse_mode='HTML')
     else:
-        context.bot.send_message(chat_id=update.message.chat_id, text='Error. Subscription failed.')
+        context.bot.send_message(chat_id=update.message.chat_id, text='Error. You need to specify which website you want to subscribe to.', parse_mode='HTML')
+
+    if webpage_object.add_chat_id(chat_id_to_add=update.message.chat_id):
+        context.bot.send_message(chat_id=update.message.chat_id, text='You have successfully been subscribed to webpage: ' + str(webpage), parse_mode='HTML')
+    else:
+        context.bot.send_message(chat_id=update.message.chat_id, text='Error. Subscription failed.', parse_mode='HTML')
+
+
+def unsubscribe(update, context):
+    webpage = ' '.join(context.args)
+    if webpage in webpages_dict:
+        webpage_object = webpages_dict[webpage]
+    elif webpage:
+        context.bot.send_message(chat_id=update.message.chat_id, text='Error. Webpage ' + str(webpage) + ' does not exist in list.', parse_mode='HTML')
+    else:
+        context.bot.send_message(chat_id=update.message.chat_id, text='Error. You need to specify which website you want to unsubscribe from.', parse_mode='HTML')
+
+    if webpage_object.remove_chat_id(update.message.chat_id):
+        context.bot.send_message(chat_id=update.message.chat_id, text='You have successfully been unsubscribed from webpage: ' + str(webpage), parse_mode='HTML')
+    else:
+        context.bot.send_message(chat_id=update.message.chat_id, text='Error. Unsubscription failed or you were not subscribed.', parse_mode='HTML')
+
+
+def active(update, context):
+    # TODO
+    # Pseudocode
+    webpages = webpages_dict.check_for_chat_id(update.message.chat_id)  # str
+
+    if webpages:
+        context.bot.send_message(chat_id=update.message.chat_id, text='You are currently subscribed to the following webpages: ' + str(webpages), parse_mode='HTML')
+    else:
+        context.bot.send_message(chat_id=update.message.chat_id, text='Error while checking subscriptions.', parse_mode='HTML')
 
 
 def stop(update, context):
+    webpages = list(webpages_dict.keys())
+    success_message = ''
+    fail_message = 0
+    for wp in webpages:
+        if webpages_dict[wp].remove_chat_id(update.message.chat_id):
+            success_message.join(webpages_dict[wp] + ' ')
+        else:
+            fail_message += 1
 
-    webpage = ' '.join(context.args)
-    webpage_object = webpages_dict[webpage]
-    if webpage_object.remove_chat_id(update.message.chat_id):
-        context.bot.send_message(chat_id=update.message.chat_id, text='You have unsubscribed from webpage: ' + webpage)
-    else:
-        context.bot.send_message(chat_id=update.message.chat_id, text='Error. You were not subscribed.')
+    if success_message:
+        context.bot.send_message(chat_id=update.message.chat_id, text='You have successfully been unsubscribed from webpages: ' + str(success_message), parse_mode='HTML')
+    elif fail_message:
+        context.bot.send_message(chat_id=update.message.chat_id, text='Error. Some unsubscriptions failed or you were not subscribed to any webpages.', parse_mode='HTML')
 
 
 def unknown(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text="Sorry, I didn't understand that command.")
+    context.bot.send_message(chat_id=update.message.chat_id, text='Sorry, I did not understand that command.', parse_mode='HTML')
 
 
 def handler(chat_id, message):
-    if(message==""): return
+    if not(message):
+        return
     bot.send_message(chat_id=chat_id, text=message)
 
 
+webpages_dict = {}
+
 updater = Updater(token='***REMOVED***', use_context=True)
 dispatcher = updater.dispatcher
-bot = telegram.Bot(token= '***REMOVED***')
-
-#filter_webpage = FilterWebpages()
+bot = telegram.Bot(token='***REMOVED***')
 
 dispatcher.add_handler(CommandHandler('start', start))
 dispatcher.add_handler(CommandHandler('subscribe', subscribe))
+dispatcher.add_handler(CommandHandler('unsubscribe', unsubscribe))
+dispatcher.add_handler(CommandHandler('active', active))
 dispatcher.add_handler(CommandHandler('stop', stop))
-
-dispatcher.add_handler(MessageHandler(Filters.command, unknown))  # needs to be added last
+# The "unknown" handler needs to be added last:
+dispatcher.add_handler(MessageHandler(Filters.command, unknown))
 
 updater.start_polling()
 
+# Use this command in the python console to clean up the Telegram service:
 # updater.stop()
