fcd70e25d31119323258594e986efcef82b0b340
Tassilo Schwarz <black@TS-MacBook-Air-2.local>
2019-05-30 14:57:05 +0200

    enhanced test

--- main_driver test.py
+++ main_driver test.py
@@ -1,10 +1,10 @@
 from main_driver import preprocess_string
 
-import telegramService
-
 # test 1
 my_str="Hellö. My naäe ist test.  Test4\n\ntest5. .fdfä.ä."
 res = preprocess_string(my_str)
 
 print("Result: "+str(res))
+assert(str(res) == "['Hello', 'My naae ist test', ' Test4', 'test5', '.fdfa.a.']")
+print("assert ok.\n\n")
 
