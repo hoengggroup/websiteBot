6aaec72e7695cad3cb664624247c4a67f4ea3d8f
Niklas Bogensperger <bogensperger.niklas@gmail.com>
2020-04-17 17:59:27 +0200

    Update loggerConfig.py
    New features:
     - Aligned by columns for better readability (might be a bit buggy due to usage of the textwrap library?)
    - Only one function for creating a logger is left, which now takes a string as argument (should in some way correspond to the name of the module that creates this logger)

--- loggerConfig.py
+++ loggerConfig.py
@@ -1,78 +1,75 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import textwrap  # for aligning the log message into a column
 
 
-def create_logger_main_driver():
+class WrappedFixedIndentingLog(logging.Formatter):
+    def __init__(self, fmt=None, datefmt=None, style='%', width=192, indent=45):
+        super().__init__(fmt=fmt, datefmt=datefmt, style=style)
+        self.wrapper = textwrap.TextWrapper(width=width, subsequent_indent=' '*indent)
+    def format(self, record):
+        return self.wrapper.fill(super().format(record))
+
+
+class ModuleFilterMain(logging.Filter):
+    def filter(self, record):
+        record.module_tag = "[MAIN]:"
+        return True
+
+
+class ModuleFilterDB(logging.Filter):
+    def filter(self, record):
+        record.module_tag = "[DB]:"
+        return True
+
+
+class ModuleFilterTG(logging.Filter):
+    def filter(self, record):
+        record.module_tag = "[TG]:"
+        return True
+
+
+class ModuleFilterVPN(logging.Filter):
+    def filter(self, record):
+        record.module_tag = "[VPN]:"
+        return True
+
+
+def create_logger(module):
     # create logger
-    logger = logging.getLogger('server_log')
+    logger = logging.getLogger(str(module)+"_log")
     logger.setLevel(logging.DEBUG)
+
     # create file handler and set it to DEBUG level
-    fh = logging.FileHandler('server_debug.log')
+    fh = logging.FileHandler(str(module)+"_debug.log")
     fh.setLevel(logging.DEBUG)
     # create file handler and set it to INFO level
-    fh2 = logging.FileHandler('server_info.log')
+    fh2 = logging.FileHandler(str(module)+"_info.log")
     fh2.setLevel(logging.INFO)
     # create console handler and set it to DEBUG level
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
-    # create formatter and add it to the handlers
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # add filter depending on the module
+    if module == "main":
+        logger.addFilter(ModuleFilterMain())
+    elif module == "db":
+        logger.addFilter(ModuleFilterDB())
+    elif module == "tg":
+        logger.addFilter(ModuleFilterTG())
+    elif module == "vpn":
+        logger.addFilter(ModuleFilterVPN())
+    else:
+        del logger
+        return
+
+    # create (custom) formatter and add it to the handlers
+    formatter = WrappedFixedIndentingLog("%(asctime)s - %(levelname)-8s - %(module_tag)-7s %(message)s", width=192, indent=45)
     fh.setFormatter(formatter)
     fh2.setFormatter(formatter)
     ch.setFormatter(formatter)
-    # add the handlers to the logger
-    logger.addHandler(fh)
-    logger.addHandler(fh2)
-    logger.addHandler(ch)
 
-    return logger
-
-
-def create_logger_telegram():
-    # create logger
-    logger = logging.getLogger('telegram_log')
-    logger.setLevel(logging.DEBUG)
-    # create file handler and set it to DEBUG level
-    fh = logging.FileHandler('telegram_debug.log')
-    fh.setLevel(logging.DEBUG)
-    # create file handler and set it to INFO level
-    fh2 = logging.FileHandler('telegram_info.log')
-    fh2.setLevel(logging.INFO)
-    # create console handler and set it to DEBUG level
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    # create formatter and add it to the handlers
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fh.setFormatter(formatter)
-    fh2.setFormatter(formatter)
-    ch.setFormatter(formatter)
-    # add the handlers to the logger
-    logger.addHandler(fh)
-    logger.addHandler(fh2)
-    logger.addHandler(ch)
-
-    return logger
-
-
-def create_logger_vpn():
-    # create logger
-    logger = logging.getLogger('vpn_log')
-    logger.setLevel(logging.DEBUG)
-    # create file handler and set it to DEBUG level
-    fh = logging.FileHandler('vpn_debug.log')
-    fh.setLevel(logging.DEBUG)
-    # create file handler and set it to INFO level
-    fh2 = logging.FileHandler('vpn_info.log')
-    fh2.setLevel(logging.INFO)
-    # create console handler and set it to DEBUG level
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    # create formatter and add it to the handlers
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fh.setFormatter(formatter)
-    fh2.setFormatter(formatter)
-    ch.setFormatter(formatter)
     # add the handlers to the logger
     logger.addHandler(fh)
     logger.addHandler(fh2)
