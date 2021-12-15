720de78db860b429660bd2a4c4bcaf5a5c0bbf3a
Niklas Bogensperger <niklasbogensperger@users.noreply.github.com>
2021-12-15 16:46:23 +0100

    configService: preparation for version 6.0
    - bump version to 6.0
    - remove filters; now handled by database
    - psycopg3 uses the postgresql url schema as one single string argument for the connect() function, so we switched to that

--- configService.py
+++ configService.py
@@ -1,18 +1,13 @@
 # -*- coding: utf-8 -*-
 
 # MAIN PARAMETERS
-version_code = "5.6.3"
+version_code = "6.0"
 keep_website_history = True
-filter_dict = {'living': ['17.509', '17.515', '13.613', '13.615', '13.617', '13.619',
-                          '17.511', '17.513', '17.503', '17.505', '17.507', '13.605', '13.607', '13.609', '13.611']}
 
 
 # DATABASE PARAMETERS
-my_database = "websitebot_db"
-my_user = "websitebot"
-my_password = "webSiteBotPostGresQL"
-my_host = "localhost"
-my_port = "5432"
+# url schema: postgresql://user:password@netloc:port/dbname
+pg_string = "postgresql://websitebot:webSiteBotPostGresQL@localhost:5432/websitebot_db"
 
 
 # REQUESTS PARAMETERS
