886de3f48293e52687622cb8474b9d6cb8107f0e
Niklas Bogensperger <niklasbogensperger@users.noreply.github.com>
2021-12-15 15:39:33 +0100

    requestsService: preparation for version 6.0
    - better error printouts/telegram messages with traceback library (implemented in logging module)
    - more elegant and versatile handling of requests errors
    - use .format() string formatting instead of concatenation like "" + str() + ""

--- requestsService.py
+++ requestsService.py
@@ -2,14 +2,13 @@
 
 # PYTHON BUILTINS
 from datetime import datetime  # for timestamps
-import sys  # for errors
 
 # EXTERNAL LIBRARIES
 import requests  # for internet traffic
 
 # OUR OWN LIBRARIES
 from configService import my_timeout, my_headers, my_verify, notify_threshold_strict, notify_threshold_permissive
-from loggerService import create_logger
+from loggerService import create_logger, exception_printing
 import databaseService as dbs
 import telegramService as tgs
 
@@ -22,66 +21,40 @@ error_states = {}
 
 
 def update_database_with_error(ws_name, error):
-    error_msg_success = dbs.db_websites_set_data(ws_name=ws_name, field="last_error_msg", argument=str(error))
+    error_msg_success = dbs.db_websites_set_data(ws_name=ws_name, field="last_error_msg", argument=error)
     error_time_success = dbs.db_websites_set_data(ws_name=ws_name, field="last_error_time", argument=datetime.now())
     if not all([error_msg_success, error_time_success]):
-        logger.warning("One or more database updates failed for website " + str(ws_name) + ". It was probably deleted from the database in the meantime.")
+        logger.warning("One or more database updates failed for website {}. It was probably deleted from the database in the meantime.".format(ws_name))
 
 
 def get_url(url, ws_name=None, timeout=my_timeout):
-    global error_states
+    global error_states  # pylint: disable=global-variable-not-assigned
     if url not in error_states:
         error_states[url] = 0
     response = None
     try:
-        logger.debug("Getting " + str(url))
+        logger.debug("Getting {}".format(url))
         response = requests.get(url, timeout=timeout, headers=my_headers, verify=my_verify)
         response.raise_for_status()
         error_states[url] = 0
-    except requests.exceptions.HTTPError as e:
-        logger.error("An HTTPError has occured while getting " + str(url))
-        logger.error("The error is: " + str(e))
-        error_states[url] += 1
-        if error_states[url] == notify_threshold_strict:
-            tgs.send_admin_broadcast("An HTTPError has occured while getting " + str(url))
-        if ws_name:
-            update_database_with_error(ws_name, e)
-        pass
-    except requests.exceptions.ConnectionError as e:
-        logger.error("A ConnectionError has occured while getting " + str(url))
-        logger.error("The error is: " + str(e))
-        error_states[url] += 1
-        if error_states[url] == notify_threshold_strict:
-            tgs.send_admin_broadcast("A ConnectionError has occured while getting " + str(url))
-        if ws_name:
-            update_database_with_error(ws_name, e)
-        pass
-    except requests.exceptions.Timeout as e:
-        logger.error("A timeout has occured while getting " + str(url))
-        logger.error("The error is: " + str(e))
-        error_states[url] += 1
-        if error_states[url] == notify_threshold_permissive:
-            tgs.send_admin_broadcast("A timeout has occured while getting " + str(url))
-        if ws_name:
-            update_database_with_error(ws_name, e)
-        pass
     except requests.exceptions.RequestException as e:
-        logger.error("An unknown RequestException has occured while getting " + str(url))
-        logger.error("The error is: " + str(e))
+        exctype, exc, tb = exception_printing(e)
+        logger.error("A requests.{} has occured in the requests module while getting {}\nError message: {}\nTraceback:\n{}".format(exctype, url, exc, tb))
         error_states[url] += 1
-        if error_states[url] == notify_threshold_strict:
-            tgs.send_admin_broadcast("An unknown RequestException has occured while getting " + str(url))
+        if error_states[url] == notify_threshold_strict and exctype != "Timeout":
+            tgs.send_admin_broadcast("A requests.{} has occured in the requests module while getting {}\nError message: {}\nTraceback:\n{}".format(exctype, url, tgs.convert_less_than_greater_than(exc), tgs.convert_less_than_greater_than(tb)))
+        elif error_states[url] == notify_threshold_permissive and exctype == "Timeout":
+            tgs.send_admin_broadcast("A requests.{} has occured in the requests module while getting {}\nError message: {}\nTraceback:\n{}".format(exctype, url, tgs.convert_less_than_greater_than(exc), tgs.convert_less_than_greater_than(tb)))
         if ws_name:
-            update_database_with_error(ws_name, e)
+            update_database_with_error(ws_name, "{}: {}".format(exctype, exc))
         pass
-    except Exception:
-        logger.error("An unknown exception has occured while getting " + str(url))
-        error_msg = str("Arg 0: " + str(sys.exc_info()[0]) + " Arg 1: " + str(sys.exc_info()[1]) + " Arg 2: " + str(sys.exc_info()[2]))
-        logger.error("The error is:" + error_msg)
+    except Exception as e:
+        exctype, exc, tb = exception_printing(e)
+        logger.error("A {} has occured in the requests module while getting {}\nError message: {}\nTraceback:\n{}".format(exctype, url, exc, tb))
         error_states[url] += 1
         if error_states[url] == notify_threshold_strict:
-            tgs.send_admin_broadcast("An unknown exception has occured while getting " + str(url))
+            tgs.send_admin_broadcast("A {} has occured in the requests module while getting {}\nError message: {}\nTraceback:\n{}".format(exctype, url, tgs.convert_less_than_greater_than(exc), tgs.convert_less_than_greater_than(tb)))
         if ws_name:
-            update_database_with_error(ws_name, error_msg)
+            update_database_with_error(ws_name, "{}: {}".format(exctype, exc))
         pass
     return response
