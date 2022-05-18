63c8cddf91d03d63855379fac29039b6f2acd4d2
Niklas Bogensperger <niklasbogensperger@users.noreply.github.com>
2022-05-18 17:25:35 +0200

    Increase timeout thresholds
    This might decrease the recent elevated network error rates

--- module_config.py
+++ module_config.py
@@ -15,4 +15,4 @@ my_timeout = 3
 my_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0', 'Accept': 'text/html'}
 my_verify = True
 notify_threshold_strict = 1  # all requests errors except timeouts
-notify_threshold_permissive = 3  # only timeouts
+notify_threshold_permissive = 5  # only timeouts
