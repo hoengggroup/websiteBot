8e5a657074aaf0e9f97ce3f28d6a10faead77370
Niklas Bogensperger <niklasbogensperger@users.noreply.github.com>
2022-12-30 14:59:00 +0100

    [6/?] (server rework) Reduce log spam
    Don't write all debug messages to files, the GBs of log files we have created thus far are too much.
    Log files will now only log from the INFO level upward, while transient console output is still set to DEBUG.

--- module_logging.py
+++ module_logging.py
@@ -63,8 +63,8 @@ def create_logger(module):
     logger.setLevel(logging.DEBUG)
 
     # create file handler and set it to DEBUG level
-    fh1 = logging.FileHandler(str(module) + "_debug.log")
-    fh1.setLevel(logging.DEBUG)
+    # fh1 = logging.FileHandler(str(module) + "_debug.log")
+    # fh1.setLevel(logging.DEBUG)
     # create file handler and set it to INFO level
     fh2 = logging.FileHandler(str(module) + "_info.log")
     fh2.setLevel(logging.INFO)
@@ -93,13 +93,13 @@ def create_logger(module):
 
     # create (custom) formatter and add it to the handlers
     formatter = WrappedFixedIndentingLog("%(asctime)s - %(levelname)-8s - %(module_tag)-8s %(message)s", width=176, indent=46)
-    fh1.setFormatter(formatter)
+    # fh1.setFormatter(formatter)
     fh2.setFormatter(formatter)
     fh3.setFormatter(formatter)
     ch.setFormatter(formatter)
 
     # add the handlers to the logger
-    logger.addHandler(fh1)
+    # logger.addHandler(fh1)
     logger.addHandler(fh2)
     logger.addHandler(fh3)
     logger.addHandler(ch)
